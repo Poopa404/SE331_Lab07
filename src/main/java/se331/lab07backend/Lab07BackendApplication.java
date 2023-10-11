2146e5ae719104748563f166c38a25b16c147b44
NOHP404 <poopa_somth@cmu.ac.th>
2023-10-11 08:17:18 +0700

    cors try

--- src/main/java/se331/lab07backend/Lab07BackendApplication.java
+++ src/main/java/se331/lab07backend/Lab07BackendApplication.java
@@ -18,7 +18,7 @@ public class Lab07BackendApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:3000")
+						.allowedOrigins("http://44.210.137.110:3000/")
 						.exposedHeaders("x-total-count");
 			}
 		};
